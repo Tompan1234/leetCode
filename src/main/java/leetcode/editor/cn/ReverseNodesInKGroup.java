7671709959b7f30ee5f30db06b276011a9876b77
panjihang <linghao.pan@tuya.com>
2021-10-10 23:01:03 +0800

    k个一组反转链表

--- src/main/java/leetcode/editor/cn/ReverseNodesInKGroup.java
+++ src/main/java/leetcode/editor/cn/ReverseNodesInKGroup.java
@@ -17,7 +17,54 @@ public class ReverseNodesInKGroup{
  */
 class Solution {
     public ListNode reverseKGroup(ListNode head, int k) {
+
+        ListNode protect = new ListNode(0, head);
+        ListNode last = protect;
+
+        while (head != null){
+            //1.分组，一组的结尾就是下一组的开头pre
+            ListNode end = getEnd(head, k);
+            if(end == null){
+                //end为null，不够一组，跳出不反转
+                break;
+            }
+            //下一组的头节点
+            ListNode nextGroupHead = end.next;
+            //2.各个组反转
+            reverseList(head,nextGroupHead);
+
+            //3.更新前一组后一组的next，pre
+            last.next = end;
+            head.next = nextGroupHead;
+            last = head;
+            head = nextGroupHead;
+
+        }
+        return protect.next;
+    }
+
+
+    private ListNode getEnd(ListNode root, int k){
+        while (root != null){
+            k--;
+            if(k==0){
+                return root;
+            }
+            root = root.next;
+        }
         return null;
+    }
+
+    //反转的是一组，所以要有head，和end
+    private void reverseList(ListNode head,ListNode end){
+        ListNode last = head;
+        head = head.next;
+        while (head != end){
+            ListNode nextHead = head.next;
+            head.next = last;
+            last = head;
+            head = nextHead;
+        }
     }
 }
 //leetcode submit region end(Prohibit modification and deletion)
