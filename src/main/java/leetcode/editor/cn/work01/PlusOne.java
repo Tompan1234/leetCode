6c2083f7842ea73d5057a82ae59297fcdef033b5
panjihang <linghao.pan@tuya.com>
2021-10-11 10:14:48 +0800

    加一

--- src/main/java/leetcode/editor/cn/work01/PlusOne.java
+++ src/main/java/leetcode/editor/cn/work01/PlusOne.java
@@ -1,13 +1,35 @@
 package leetcode.editor.cn.work01;
 
+import javax.jws.Oneway;
+import javax.lang.model.element.VariableElement;
+import java.util.Stack;
+import java.util.Vector;
+
 public class PlusOne{
     public static void main(String[] args) {
            Solution solution = new PlusOne().new Solution();
-      }
+           int[] a = {9,9};
+           int[] objects = solution.plusOne(a);
+           for(int i = 0; i< objects.length;i++){
+               System.out.println(objects[i]);
+           }
+    }
 //leetcode submit region begin(Prohibit modification and deletion)
 class Solution {
     public int[] plusOne(int[] digits) {
-        return new int[1];
+        //倒序+1，考虑到进位
+        for(int i =digits.length-1; i>=0;i--){
+            digits[i]++;
+            digits[i] %= 10;
+            if (digits[i]!=0) {
+                //位数不变
+                return digits;
+            }
+        }
+        //进位增加位数
+        digits = new int[digits.length + 1];
+        digits[0] = 1;
+        return digits;
     }
 }
 //leetcode submit region end(Prohibit modification and deletion)
