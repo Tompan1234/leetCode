db45f6fcb77ca7dbbb888c004373f3fd0fa64ba0
panjihang <linghao.pan@tuya.com>
2021-10-14 00:02:14 +0800

    lru缓存

--- src/main/java/leetcode/editor/cn/LruCache.java
+++ src/main/java/leetcode/editor/cn/LruCache.java
@@ -13,6 +13,7 @@ class LRUCache {
     private LinkedNode start,end;
     private Map<Integer,LinkedNode> map = new HashMap<>();
 
+    //在双向链表的实现中，使用一个伪头部（dummy head）和伪尾部（dummy tail）标记界限，这样在添加节点和删除节点的时候就不需要检查相邻的节点是否存在。
     public LRUCache(int capacity) {
         this.capacity = capacity;
         start = new LinkedNode();
@@ -20,7 +21,13 @@ class LRUCache {
         start.next = end;
         end.pre = start;
     }
-    
+
+    /**
+     * 如果 key 不存在，则返回 -1−1；
+     * 如果 key 存在，则 key 对应的节点是最近被使用的节点。通过哈希表定位到该节点在双向链表中的位置，并将其移动到双向链表的头部，最后返回该节点的值。
+     * @param key
+     * @return
+     */
     public int get(int key) {
         if(map.containsKey(key)){
             LinkedNode linkedNode = map.get(key);
@@ -29,7 +36,14 @@ class LRUCache {
         }
         return -1;
     }
-    
+
+
+    /**
+     * 如果 key 不存在，使用 key 和 value 创建一个新的节点，在双向链表的头部添加该节点，并将 key 和该节点添加进哈希表中。然后判断双向链表的节点数是否超出容量，如果超出容量，则删除双向链表的尾部节点，并删除哈希表中对应的项。
+     * 如果 key 存在，则与 get 操作类似，先通过哈希表定位，再将对应的节点的值更新为 value，并将该节点移到双向链表的头部。
+     * @param key
+     * @param value
+     */
     public void put(int key, int value) {
         if(map.containsKey(key)){
             LinkedNode linkedNode = map.get(key);
@@ -58,6 +72,13 @@ class LRUCache {
         }
     }
 
+    /**
+     * 注意前后指针指向
+     *          <--             <--
+     *  start            node            end
+     *          -->             -->
+     * @param node
+     */
     private void addToStart(LinkedNode node){
         node.pre = start;
         node.next = start.next;
