bbac8ba420184addc61eae384cb0e058436c8a45
panjihang <pan.jihang@car1768.com>
2024-11-11 22:17:40 +0800

    添加删除链表倒数第N个节点的解决方案

--- src/main/java/leetcode/editor/cn/SLwz0R.java
+++ src/main/java/leetcode/editor/cn/SLwz0R.java
@@ -1,4 +1,9 @@
 package leetcode.editor.cn;
+
+public class SLwz0R{
+    public static void main(String[] args) {
+           Solution solution = new SLwz0R().new Solution();
+      }
 //leetcode submit region begin(Prohibit modification and deletion)
 /**
  * Definition for singly-linked list.
@@ -12,7 +17,21 @@ package leetcode.editor.cn;
  */
 class Solution {
     public ListNode removeNthFromEnd(ListNode head, int n) {
-
+        ListNode fast = head;
+        ListNode temp = new ListNode(0, head);
+        ListNode slow = temp;
+        for (int i = 0; i < n; i++) {
+            fast = fast.next;
+        }
+        while (fast != null){
+            fast = fast.next;
+            slow = slow.next;
+        }
+        slow.next = slow.next.next;
+        head = temp.next;
+        return head;
     }
 }
 //leetcode submit region end(Prohibit modification and deletion)
+
+}
